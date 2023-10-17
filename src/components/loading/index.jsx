161995bb300f217a864e1fefdc99823f285919eb
f-gabriel-braga <gabrielbraga0712@gmail.com>
2023-10-17 20:19:45 -0300

    Loading

--- src/components/loading/index.jsx
+++ src/components/loading/index.jsx
@@ -1,7 +1,12 @@
+import poke from "../../assets/poke.svg";
+import "./style.css";
+
 export default function Loading() {
   return (
-    <div className="w-screen bg-red-500 absolute flex flex-col justify-center items-center bg-opacity-50 h-screen">
-      <div className="opacity-100">Load</div>
+    <div className="w-screen bg-red-500 absolute flex flex-col justify-center items-center h-screen">
+      <div className="opacity-100">
+        <img src={poke} alt="Poke" className="rotate-animation w-24" />
+      </div>
     </div>
   );
 }
